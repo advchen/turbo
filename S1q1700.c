b94a7262807faca1eb2d7b4dcac2b967acb97e8e
advchen <1159378129@qq.com>
2024-05-29 14:12:37 +0800

    ok

---
+++ S1q1700.c
@@ -0,0 +1,24 @@
+/*************************************************************************
+	> File Name: S1q1700.c
+	> Author: 
+	> Mail: 
+	> Created Time: Wed 29 May 2024 02:12:09 PM CST
+ ************************************************************************/
+
+int countStudents(int* students, int studentsSize, int* sandwiches, int sandwichesSize) {
+    int s1 = 0;
+    for (int i = 0; i < studentsSize; i++) {
+        s1 += students[i];
+    }
+    int s0 = studentsSize - s1;
+    for (int i = 0; i < sandwichesSize; i++) {
+        if (sandwiches[i] == 0 && s0 > 0) {
+            s0--;
+        } else if (sandwiches[i] == 1 && s1 > 0) {
+            s1--;
+        } else {
+            break;
+        }
+    }
+    return s0 + s1;
+}
